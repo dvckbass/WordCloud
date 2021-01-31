04c1f9488e509076ea37b2a75c492a5d8b9d9d45
dvckbass <duckbass@live.com>
2021-01-31 16:17:41 +0100

    wordcloud image is shown

---
+++ README.md
@@ -0,0 +1,5 @@
+1. pip install wordcloud
+1. pip install fileupload
+1. pip install ipywidgets
+1. jupyter nbextension install --py --user fileupload
+1. jupyter nbextension enable --py fileupload
--- main.py
+++ main.py
@@ -1,15 +1,51 @@
-# This is a sample Python script.
+import wordcloud
+import numpy as np
+from matplotlib import pyplot as plt
+from IPython.display import display
+import fileupload
+import io
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# read file
+file = open("text.txt", "r")
+words = file.read()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def calculate_frequencies(file_contents):
+    # Here is a list of punctuations and uninteresting words
+    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+    uninteresting_words = ["the", "a", "to", "if", "is", "it", "of", "and", "or", "an", "as", "i", "me", "my", \
+                           "we", "our", "ours", "you", "your", "yours", "he", "she", "him", "his", "her", "hers", "its",
+                           "they", "them", \
+                           "their", "what", "which", "who", "whom", "this", "that", "am", "are", "was", "were", "be",
+                           "been", "being", \
+                           "have", "has", "had", "do", "does", "did", "but", "at", "by", "with", "from", "here", "when",
+                           "where", "how", \
+                           "all", "any", "both", "each", "few", "more", "some", "such", "no", "nor", "too", "very",
+                           "can", "will", "just"]
 
+    wordDict = {}
+    for word in file_contents.split():
+        word = word.lower()
+        if word not in punctuations and word not in uninteresting_words:
+            if word[-1:] in punctuations:
+                word = word[:-1]
+            if word[:1] in punctuations:
+                word = word[1:]
+            if word.isalpha():
+                if word not in wordDict:
+                    wordDict[word] = 1
+                else:
+                    wordDict[word] += 1
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    # wordcloud
+    cloud = wordcloud.WordCloud()
+    cloud.generate_from_frequencies(wordDict)
+    return cloud.to_array()
 
+# Display wordcloud image
+
+myimage = calculate_frequencies(words)
+plt.imshow(myimage, interpolation = 'nearest')
+plt.axis('off')
+plt.show()
